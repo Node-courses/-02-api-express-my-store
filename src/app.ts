59617f49e89e9aacb2d85d2e185e21da42f34936
AlejoDev <alejodev95@gmail.com>
2024-11-16 18:45:06 -0500

    Instalando express y primer servidor HTTP

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
+import express from "express";
 
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola desde express");
+});
+
+app.listen(port, () => {
+  console.log("Escuchando en el puerto", port);
+});
