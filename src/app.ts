32a0dc0b6a8a29704d420b89ec4ee1435689ff2c
AlejoDev <alejodev95@gmail.com>
2024-11-17 06:49:58 -0500

    creación de nuevas rutas

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.send("Hola desde express");
 });
 
+app.get("/nueva-ruta", (req, res) => {
+  res.send("Esta es una nueva ruta");
+});
+
+app.get("/products", (req, res) => {
+  res.json({
+    name: "Product 1",
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log("Escuchando en el puerto", port);
 });
